4d7be8c7735e53bb0725d64ac8d4209421d080da
HusnainK721 <husnaink721@gmail.com>
2021-08-21 15:34:10 +0530

    new

--- src/component/Productlist.jsx
+++ src/component/Productlist.jsx
@@ -1,22 +1,26 @@
 import React from "react";
 import { Card, Row, Col } from "react-bootstrap";
 import ProductItem from "./ProductItem";
-import "../component/Product.css"
+import "../component/Product.css";
+import { useSelector } from "react-redux";
+import { useState } from "react";
 
 export const Productlist = () => {
+  const myProducts = useSelector((state) => state.productReducers.product);
+  const [prod, setProd] = useState([...myProducts]); 
   return (
-      <Card className="productlist">
-    <Row>
-      <Col>
-        <ProductItem />
-      </Col>
-      <Col>
-        <ProductItem />
-      </Col>
-      <Col>
-        <ProductItem />
-      </Col>
-    </Row>
+    <Card className="productlist">
+      <Row>
+        <Col>
+          <ProductItem />
+        </Col>
+        <Col>
+          <ProductItem />
+        </Col>
+        <Col>
+          <ProductItem />
+        </Col>
+      </Row>
     </Card>
   );
 };
