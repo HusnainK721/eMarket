b2185e8be28f08a37bb45f198e18bd69c99d2556
HusnainK721 <husnaink721@gmail.com>
2021-08-23 16:32:24 +0530

    finish

--- src/component/Productlist.jsx
+++ src/component/Productlist.jsx
@@ -4,9 +4,17 @@ import { Card, Row, Col } from "react-bootstrap";
 import { ProductItem } from "./ProductItem";
 import { useEffect } from "react";
 
-export const ProductList = () => {
+export const ProductList = ({ searchTxt }) => {
   const myProducts = useSelector((state) => state.productReducer.product);
   const [prods, setProds] = useState([]);
+  useEffect(() => {
+    if (searchTxt !== "") {
+      const filteredproducts = myProducts.filter((item) =>
+        item.title.toUpperCase().includes(searchTxt.toUpperCase())
+      );
+      setProds([...filteredproducts]);
+    }
+  }, [searchTxt]);
 
   useEffect(() => {
     setProds([...myProducts]);
