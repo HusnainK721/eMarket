72c7b6447ee907f3a3535351dcacaae2952cac4b
HusnainK721 <husnaink721@gmail.com>
2021-08-24 12:09:58 +0530

    top nav new

--- src/component/TopNav.jsx
+++ src/component/TopNav.jsx
@@ -11,11 +11,19 @@ export const TopNav = ({ setSearchTxt }) => {
         <Col md={8}>
           <Navbar bg="dark" variant="dark">
             <Container>
-              <Navbar.Brand href="#home">Ecart</Navbar.Brand>
+              <Navbar.Brand href="#home">
+                <img
+                  alt=""
+                  src="kk2.png"
+                  width="60"
+                  height="30"
+                  className="d-inline-block align-top"
+                />{" "}
+              </Navbar.Brand>
               <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="/">Home</Nav.Link>
                 <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
+                <Nav.Link href="#pricing">About Us</Nav.Link>
               </Nav>
             </Container>
           </Navbar>
