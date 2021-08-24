a8f939a217af0d91aa66202942d70060ee27ff32
HusnainK721 <husnaink721@gmail.com>
2021-08-24 12:12:25 +0530

    new

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Provider } from "react-redux";
 import { configStore } from "./state/store/configStore";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Home } from "./component/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 function App() {
   const localStore = configStore();
   return (
-    <div>
+    
+      <div>
       <Provider store={localStore}>
         <Home />
       </Provider>
